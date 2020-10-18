55bc5935d8472f460464f7cf7aa633e138558f58
Matheus <matcatarino@yahoo.com.br>
2020-10-18 13:16:14 -0300

    removed lib from github

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import 'reflect-metadata';
-export { get, post, put, del, patch, next } from './Decorators';
-export { loadRoute } from './loadRoute';
--- lib/loadRoute.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const loadRoute: (routeInstance: any) => any;
